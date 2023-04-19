245a2a6eb168fd4ba29c6c37b4c606b253c8b44e
d0wnsider <jmhandsamson@gmail.com>
2023-04-19 18:41:06 -0400

    Update contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,12 +2,14 @@ function createContact() {
   const body = document.querySelector("body");
   const content = document.querySelector(".content");
   const footer = document.querySelector("footer");
+  content.classList.add("contactUpdate");
   if (content) {
     content.remove();
   }
 
   const newContent = document.createElement("div");
   newContent.classList.add("content");
+  newContent.classList.add("contactUpdate");
 
   const addressTitle = document.createElement("h1");
   addressTitle.textContent = "Address";
@@ -22,9 +24,19 @@ function createContact() {
     "Monday: 12:00 - 22:00 PM Tuesday: 12:00 - 22:00 PM Wednesday: 12:00 - 22:00 PM Thursday: 12:00 - 22:00 PM Friday: 12:00 - 22:00 PM Saturday: 12:00 - 22:00 PM";
   hours.classList.add("hours");
 
+  const telephone = document.createElement("h1");
+  telephone.textContent = "Telephone";
+  telephone.classList.add("telephone");
+
+  const telephoneDesc = document.createElement("p");
+  telephoneDesc.textContent = "123-456-78-90";
+  telephoneDesc.classList.add("telephoneDesc");
+
   newContent.appendChild(addressTitle);
   newContent.appendChild(addressDesc);
   newContent.appendChild(hours);
+  newContent.appendChild(telephone);
+  newContent.appendChild(telephoneDesc);
   body.appendChild(newContent);
 
   if (footer) {
