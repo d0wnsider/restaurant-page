254cef0b1c7121148a545f0931ce29761be85896
d0wnsider <jmhandsamson@gmail.com>
2023-04-19 18:25:59 -0400

    Update menu page

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,6 +1,11 @@
+import strawberry from "../img/strawberry.png";
+import banana from "../img/banana.png";
+import kale from "../img/kale.png";
+
 function createMenu() {
   const body = document.querySelector("body");
   const content = document.querySelector(".content");
+  content.classList.add("contentBlur");
   const footer = document.querySelector("footer");
   if (content) {
     content.remove();
@@ -8,10 +13,14 @@ function createMenu() {
 
   const newContent = document.createElement("div");
   newContent.classList.add("content");
+  newContent.classList.add("contentBlur");
 
   // strawberry
   const sCard = document.createElement("div");
   sCard.classList.add("sCard");
+  const sImg = new Image();
+  sImg.src = strawberry;
+  sImg.style.width = "150px";
   const sTitle = document.createElement("h1");
   sTitle.textContent = "Strawberry Smoothie";
   sTitle.classList.add("sTitle");
@@ -22,6 +31,9 @@ function createMenu() {
   // banana
   const bCard = document.createElement("div");
   bCard.classList.add("bCard");
+  const bImg = new Image();
+  bImg.src = banana;
+  bImg.style.width = "150px";
   const bTitle = document.createElement("h1");
   bTitle.textContent = "Banana Smoothie";
   bTitle.classList.add("bTitle");
@@ -32,6 +44,9 @@ function createMenu() {
   // mango
   const kCard = document.createElement("div");
   kCard.classList.add("kCard");
+  const kImg = new Image();
+  kImg.src = kale;
+  kImg.style.width = "150px";
   const kTitle = document.createElement("h1");
   kTitle.textContent = "Kale Smoothie";
   kTitle.classList.add("kTitle");
@@ -39,10 +54,13 @@ function createMenu() {
   kDesc.classList.add("kDesc");
   kDesc.textContent = "Pure plant based";
 
+  sCard.appendChild(sImg);
   sCard.appendChild(sTitle);
   sCard.appendChild(sDesc);
+  bCard.appendChild(bImg);
   bCard.appendChild(bTitle);
   bCard.appendChild(bDesc);
+  kCard.appendChild(kImg);
   kCard.appendChild(kTitle);
   kCard.appendChild(kDesc);
 
