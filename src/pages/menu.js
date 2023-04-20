b0d266552275c6be47e8501efa83942d9bd5bb78
d0wnsider <jmhandsamson@gmail.com>
2023-04-19 20:32:26 -0400

    Update menu page

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,6 +1,9 @@
 import strawberry from "../img/strawberry.png";
 import banana from "../img/banana.png";
 import kale from "../img/kale.png";
+import watermelon from "../img/watermelon.png";
+import blueberry from "../img/blueberry.png";
+import avocado from "../img/avocado.png";
 
 function createMenu() {
   const body = document.querySelector("body");
@@ -10,6 +13,10 @@ function createMenu() {
   if (content) {
     content.remove();
   }
+
+  const title = document.createElement("p");
+  title.textContent = "Smoothies";
+  title.classList.add("menuTitle");
 
   const newContent = document.createElement("div");
   newContent.classList.add("content");
@@ -22,7 +29,7 @@ function createMenu() {
   sImg.src = strawberry;
   sImg.style.width = "150px";
   const sTitle = document.createElement("h1");
-  sTitle.textContent = "Strawberry Smoothie";
+  sTitle.textContent = "Strawberry Smoothie - $12.99";
   sTitle.classList.add("sTitle");
   const sDesc = document.createElement("p");
   sDesc.classList.add("sDesc");
@@ -35,7 +42,7 @@ function createMenu() {
   bImg.src = banana;
   bImg.style.width = "150px";
   const bTitle = document.createElement("h1");
-  bTitle.textContent = "Banana Smoothie";
+  bTitle.textContent = "Banana Smoothie - $12.99";
   bTitle.classList.add("bTitle");
   const bDesc = document.createElement("p");
   bDesc.classList.add("bDesc");
@@ -48,11 +55,50 @@ function createMenu() {
   kImg.src = kale;
   kImg.style.width = "150px";
   const kTitle = document.createElement("h1");
-  kTitle.textContent = "Kale Smoothie";
+  kTitle.textContent = "Kale Smoothie - $12.99";
   kTitle.classList.add("kTitle");
   const kDesc = document.createElement("p");
   kDesc.classList.add("kDesc");
   kDesc.textContent = "Pure plant based";
+
+  // watermelon
+  const wCard = document.createElement("div");
+  wCard.classList.add("wCard");
+  const wImg = new Image();
+  wImg.src = watermelon;
+  wImg.style.width = "150px";
+  const wTitle = document.createElement("h1");
+  wTitle.textContent = "Watermelon Smoothie - $12.99";
+  wTitle.classList.add("wTitle");
+  const wDesc = document.createElement("p");
+  wDesc.classList.add("sDesc");
+  wDesc.textContent = "Light and Creamy texture";
+
+  // blueberry
+  const bbCard = document.createElement("div");
+  bbCard.classList.add("bbCard");
+  const bbImg = new Image();
+  bbImg.src = blueberry;
+  bbImg.style.width = "150px";
+  const bbTitle = document.createElement("h1");
+  bbTitle.textContent = "Blueberry Smoothie - $12.99";
+  bbTitle.classList.add("bbTitle");
+  const bbDesc = document.createElement("p");
+  bbDesc.classList.add("bbDesc");
+  bbDesc.textContent = "Sweet and flavorful";
+
+  // avocado
+  const aCard = document.createElement("div");
+  aCard.classList.add("aCard");
+  const aImg = new Image();
+  aImg.src = avocado;
+  aImg.style.width = "150px";
+  const aTitle = document.createElement("h1");
+  aTitle.textContent = "Avocado Smoothie - $12.99";
+  aTitle.classList.add("aTitle");
+  const aDesc = document.createElement("p");
+  aDesc.classList.add("aDesc");
+  aDesc.textContent = "Pure plant based";
 
   sCard.appendChild(sImg);
   sCard.appendChild(sTitle);
@@ -63,10 +109,23 @@ function createMenu() {
   kCard.appendChild(kImg);
   kCard.appendChild(kTitle);
   kCard.appendChild(kDesc);
+  wCard.appendChild(wImg);
+  wCard.appendChild(wTitle);
+  wCard.appendChild(wDesc);
+  bbCard.appendChild(bbImg);
+  bbCard.appendChild(bbTitle);
+  bbCard.appendChild(bbDesc);
+  aCard.appendChild(aImg);
+  aCard.appendChild(aTitle);
+  aCard.appendChild(aDesc);
 
+  newContent.appendChild(title);
   newContent.appendChild(sCard);
   newContent.appendChild(bCard);
   newContent.appendChild(kCard);
+  newContent.appendChild(wCard);
+  newContent.appendChild(bbCard);
+  newContent.appendChild(aCard);
   body.appendChild(newContent);
 
   if (footer) {
